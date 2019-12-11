236f1e8ebea90683dbdc5c0fed097662cbef9bd9
ZIDAT <zidatsara@gmail.com>
2019-12-11 17:36:21 +0100

    dbResponse modification

--- API/src/modules/drinks/services/createOne.js
+++ API/src/modules/drinks/services/createOne.js
@@ -14,5 +14,6 @@ module.exports = (drinkToCreate) => {
       return connect()
             .then(db => db.collection(collections.DRINKS))
             .then(collection => collection.insertOne(drink))
+            .then(dbResponse => dbResponse.ops[0]);
         });
 }
